580d2ceb67d8d39bfcf5f92614c0ee53a500efbd
AkankshaNarula <64724811+AkankshaNarula@users.noreply.github.com>
2024-07-15 13:05:01 +0530

    Update homepage.dart

--- Myntra/lib/homepage.dart
+++ Myntra/lib/homepage.dart
@@ -1,146 +1,206 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'home_page_model.dart';
+export 'home_page_model.dart';
 
 class HomePageWidget extends StatefulWidget {
-  const HomePageWidget({Key? key}) : super(key: key);
+  const HomePageWidget({super.key});
 
   @override
   State<HomePageWidget> createState() => _HomePageWidgetState();
 }
 
 class _HomePageWidgetState extends State<HomePageWidget> {
-  late TextEditingController _textFieldController;
+  late HomePageModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
     super.initState();
-    _textFieldController = TextEditingController();
+    _model = createModel(context, () => HomePageModel());
   }
 
   @override
   void dispose() {
-    _textFieldController.dispose();
+    _model.dispose();
+
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        if (_textFieldController.text.isNotEmpty) {
-          FocusScope.of(context).unfocus();
-        }
-      },
+      onTap: () => _model.unfocusNode.canRequestFocus
+          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+          : FocusScope.of(context).unfocus(),
       child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: Color(0xFFF1F4F8),
         appBar: AppBar(
           backgroundColor: Color(0xFF4B39EF),
+          automaticallyImplyLeading: false,
           title: Text(
             'Upcoming Contests',
-            style: TextStyle(
-              fontFamily: 'Outfit',
-              color: Colors.white,
-              fontSize: 22,
-              letterSpacing: 0,
-            ),
+            style: FlutterFlowTheme.of(context).headlineMedium.override(
+                  fontFamily: 'Outfit',
+                  color: Colors.white,
+                  fontSize: 22,
+                  letterSpacing: 0,
+                ),
           ),
+          actions: [],
           centerTitle: false,
           elevation: 2,
         ),
         body: SafeArea(
+          top: true,
           child: Column(
+            mainAxisSize: MainAxisSize.max,
             children: [
-              Container(
-                padding: EdgeInsets.all(16),
-                color: Color(0xFFF1F4F8),
-                child: Text(
-                  'Design looks for brand sponsored\n events and Win Exciting\nprices and vouchers !!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Outfit',
-                    color: Color(0xFF4B39EF),
-                    fontSize: 24,
-                    letterSpacing: 0,
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    width: 404,
+                    height: 112,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF1F4F8),
+                    ),
+                    child: Text(
+                      'Design looks for brand sponsored\n events and Win Exciting\nprices and vouchers !!',
+                      textAlign: TextAlign.center,
+                      style: FlutterFlowTheme.of(context).titleLarge.override(
+                            fontFamily: 'Outfit',
+                            color: Color(0xFF4B39EF),
+                            letterSpacing: 0,
+                          ),
+                    ),
                   ),
-                ),
+                ],
               ),
-              Expanded(
-                child: ListView(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  children: [
-                    SizedBox(height: 16),
-                    Container(
-                      height: 200,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFF1F4F8),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image.asset(
-                              'assets/images/birthday.webp',
-                              width: double.infinity,
-                              height: 140,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, 'ContestRegister');
-                            },
-                            child: Text('Enter Contest >>'),
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.blue, // button's fill color
-                              onPrimary: Colors.white, // text color
-                              elevation: 3,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    width: 389,
+                    height: 256,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF1F4F8),
                     ),
-                    SizedBox(height: 16),
-                    Container(
-                      height: 200,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFF1F4F8),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          ClipRRect(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.asset(
+                            'assets/images/birthday.webp',
+                            width: 359,
+                            height: 200,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        FFButtonWidget(
+                          onPressed: () async {
+                            context.pushNamed('ContestRegister');
+                          },
+                          text: 'Enter Contest >>',
+                          options: FFButtonOptions(
+                            height: 40,
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                            iconPadding:
+                                EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                            color: FlutterFlowTheme.of(context).primary,
+                            textStyle: FlutterFlowTheme.of(context)
+                                .titleSmall
+                                .override(
+                                  fontFamily: 'Readex Pro',
+                                  color: Colors.white,
+                                  letterSpacing: 0,
+                                ),
+                            elevation: 3,
+                            borderSide: BorderSide(
+                              color: Colors.transparent,
+                              width: 1,
+                            ),
                             borderRadius: BorderRadius.circular(8),
-                            child: Image.asset(
-                              'assets/images/gym.jpg',
-                              width: double.infinity,
-                              height: 140,
-                              fit: BoxFit.cover,
-                            ),
                           ),
-                          ElevatedButton(
-                            onPressed: () {
-                              print('Button pressed ...');
-                            },
-                            child: Text('Starts in 2 hours...'),
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.blue, // button's fill color
-                              onPrimary: Colors.white, // text color
-                              elevation: 3,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 16),
-                  ],
-                ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    width: 395,
+                    height: 42,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF1F4F8),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    width: 390,
+                    height: 240,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF1F4F8),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.asset(
+                            'assets/images/gym.jpg',
+                            width: 367,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        FFButtonWidget(
+                          onPressed: () {
+                            print('Button pressed ...');
+                          },
+                          text: 'Starts in 2 hours...',
+                          options: FFButtonOptions(
+                            height: 40,
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                            iconPadding:
+                                EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                            color: FlutterFlowTheme.of(context).primary,
+                            textStyle: FlutterFlowTheme.of(context)
+                                .titleSmall
+                                .override(
+                                  fontFamily: 'Readex Pro',
+                                  color: Colors.white,
+                                  letterSpacing: 0,
+                                ),
+                            elevation: 3,
+                            borderSide: BorderSide(
+                              color: Colors.transparent,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
@@ -149,3 +209,4 @@ class _HomePageWidgetState extends State<HomePageWidget> {
     );
   }
 }
+
